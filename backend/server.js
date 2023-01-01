64a79c72edc6705cace62de9daf47f8497d359f1
scarlettcello <scarlettcello@gmail.com>
2022-12-31 20:29:23 -0500

    Fixed path in server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 const path = require('path');
-const { application } = require('express');
-const colors = require('colors');
 const express = require('express');
 const dotenv = require('dotenv').config();
+const colors = require('colors');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
@@ -20,7 +19,7 @@ app.use('/api/users', require('./routes/userRoutes'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
   
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')));
 } else {
   app.get('/', (req, res) => res.send('Please set to production'));
 }
