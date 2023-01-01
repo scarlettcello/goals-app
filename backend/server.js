1b64c46bc11617b62a27ed2e01f70843b169388a
scarlettcello <scarlettcello@gmail.com>
2022-12-31 20:40:35 -0500

    Fixed path of app.get

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'production') {
   
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')));
 } else {
-  app.get('/', (req, res) => res.send('Please set to production'));
+  app.get('/api', (req, res) => res.send('Please set to production'));
 }
 
 app.use(errorHandler);
